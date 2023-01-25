b9fb5c7adb4f48df7c9216318aee78569074155f
Asier Echauri <asieree00@gmail.com>
2023-01-25 10:24:41 +0100

    arreglar errores v0.0-2

--- app/src/main/java/com/xea/whatsappxea/fragments/ShowFragment.java
+++ app/src/main/java/com/xea/whatsappxea/fragments/ShowFragment.java
@@ -7,7 +7,7 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-import com.asier_echauri.toolbarasier.R;
+import com.xea.whatsappxea.R;
 
 /**
  * A simple {@link Fragment} subclass.
