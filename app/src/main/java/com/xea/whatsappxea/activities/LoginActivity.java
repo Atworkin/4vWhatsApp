b5ab618840f4ab52ad8d6fcf4cef0af892a8ade6
Murillo-EL-Navajas <xabiermuro1612@gmail.com>
2023-02-01 09:58:28 +0100

    recycler xml

--- app/src/main/java/com/xea/whatsappxea/activities/LoginActivity.java
+++ app/src/main/java/com/xea/whatsappxea/activities/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.xea.whatsappxea.R;
 
 public class LoginActivity extends AppCompatActivity {
