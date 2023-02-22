95eecbd782fcf0bd9a1f38d6585d6ef440e931fc
Asier Echauri <asieree00@gmail.com>
2023-02-22 10:18:40 +0100

    Group activity

---
+++ app/src/main/java/com/xea/whatsappxea/activities/GroupActivity.java
@@ -0,0 +1,15 @@
+package com.xea.whatsappxea.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.xea.whatsappxea.R;
+import android.os.Bundle;
+
+public class GroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+    }
+}
